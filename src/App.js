add65616dd8086d417d86ff4a72be92690072d89
MaRtin322s <marti.sofroniev0408@gmail.com>
2022-12-15 13:08:02 +0200

    add textarea changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <div className="box">
-        <div className="field">
-          <div className="control">
-            <textarea className="textarea is-large" placeholder="Notes..." />
-          </div>
+    const [text, setText] = useState("");
+
+    const onChange = (ev) => {
+        setText(ev.target.value);
+    }
+
+    const saveHandler = () => {
+        localStorage.setItem('text', text);
+    }
+
+    const resetHandler = () => {
+        setText("");
+    }
+
+    return (
+        <div className="App">
+            <div className="box">
+                <div className="field">
+                    <div className="control">
+                        <textarea className="textarea is-large" value={text} placeholder="Notes..." onChange={(ev) => onChange(ev)} />
+                    </div>
+                </div>
+                <button className="button is-large is-primary is-active" onClick={saveHandler}>Save</button>
+                <button className="button is-large" onClick={resetHandler}>Clear</button>
+            </div>
         </div>
-        <button className="button is-large is-primary is-active">Save</button>
-        <button className="button is-large">Clear</button>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default App;
